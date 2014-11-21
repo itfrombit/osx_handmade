9b14abb73fbcb10a783e9ecdb4adc02325a12e6d
Jeff <jsb@dragon-7.home>
2014-11-21 17:07:13 -0500

    Initial Commit

---
+++ handmade/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  handmade
+//
+//  Created by Jeff on 11/21/14.
+//  Copyright (c) 2014 Jeff. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+

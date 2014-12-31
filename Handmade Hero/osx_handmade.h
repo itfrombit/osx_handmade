354fa1534b6d529396adeee97b5a5235b15f1256
Jeff Buck <jeffreybuck@mac.com>
2014-12-31 11:12:31 -0500

    Minor changes to incorporate handmade_platform.h. Day 029.

--- Handmade Hero/osx_handmade.h
+++ Handmade Hero/osx_handmade.h
@@ -5,28 +5,6 @@
 #include <math.h>
 
 
-// NOTE(jeff): These typedefs are the same as in win32
-#define internal static
-#define local_persist static
-#define global_variable static
-
-#define Pi32 3.14159265359f
-
-typedef int8_t int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-typedef int32 bool32;
-
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-
-typedef float real32;
-typedef double real64;
-
-
 struct osx_offscreen_buffer
 {
 	// NOTE: Pixels are always 32-bits wide. BB GG RR XX
